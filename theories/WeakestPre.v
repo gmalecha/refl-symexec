7d8edc13ec6f2fec6cf0c24a2bebcaaf80af06b5
Gregory Malecha <gmalecha@cs.harvard.edu>
2014-06-04 23:32:46 -0400

    weakest pre conditions but it won't work
    - there is no value for locations, arrays and memory without a state.
    - these terms have to be put everywhere!

--- theories/WeakestPre.v
+++ theories/WeakestPre.v
@@ -1,7 +1,11 @@
+Require Import Coq.Lists.List.
 Require Import ExtLib.Core.RelDec.
 Require Import ExtLib.Structures.Monad.
 Require Import ExtLib.Data.Option.
+Require Import ExtLib.Data.Positive.
+Require Import ExtLib.Data.Bool.
 Require Import Rtl.Rtl.
+Require Import MirrorCore.SymEnv.
 Require Import MirrorCore.Lambda.Expr.
 Require Import MirrorCore.Lambda.ExprLift.
 
@@ -11,53 +15,114 @@ Set Strict Implicit.
 Section wp.
   Import MonadNotation.
 
-  Variable typ : Type.
-  Variable typD : list Type -> typ -> Type.
+  Inductive typ :=
+  | tyOpaque : positive -> typ
+  | tyInt : nat -> typ
+  | tyArray : nat -> nat -> typ
+  | tyLoc : nat -> typ
+  | tyProp
+  | tyArr : typ -> typ -> typ
+  | tyState.
+  Axiom typD : list Type -> typ -> Type.
 
-  Definition func : Type := ({ sz : nat & location sz } + nat)%type.
+  Inductive func : Type :=
+  | fOpaque : positive -> func
+  | fBVop : nat -> bit_vector_op -> func
+  | fTestop : nat -> test_op -> func
+  | fIf : typ -> func
+  | fCast : bool -> nat -> nat -> func
+  | fConst : forall sz, int sz -> func
+  | fReadable | fWriteable | fAnd | fOr | fTrue | fFalse
+  | fEx : typ -> func
+  | fEq : typ -> func
+  | fArray_upd : nat -> nat -> func
+  | fMem_upd
+  | fMem_read
+  | fPure : typ -> func
+  | fAp : typ -> typ -> func
+      (** TODO(gmalecha): These are not going to work! **)
+  | fLoc : forall sz, location sz -> func
+  | fArray : forall s l, array s l -> func
+  | fMemory
+  .
 
   Local Notation "a @ b" := (@App typ func a b) (only parsing, at level 30).
 
-  Instance func_eq : RelDec (@eq func).
-  Admitted.
+  Instance typ_eq : RelDec (@eq typ) :=
+  { rel_dec := fun a b =>
+       match a , b with
+         | tyOpaque a , tyOpaque b => a ?[ eq ] b
+         | tyInt a , tyInt b => a ?[ eq ] b
+         | tyArray a b , tyArray c d =>
+           if a ?[ eq ] c then b ?[ eq ] d else false
+         | tyLoc a , tyLoc b => a ?[ eq ] b
+         | tyProp , tyProp => true
+         | tyState , tyState => true
+         | _ , _ => false
+       end }.
+
+  Definition bv_eq_dec : forall a b : bit_vector_op, {a = b} + {a <> b}.
+    decide equality.
+  Defined.
+
+  Instance bv_op_eq : RelDec (@eq bit_vector_op) :=
+  { rel_dec := fun a b => match bv_eq_dec a b with
+                            | right _ => false
+                            | left _ => true
+                          end }.
+
+  Instance func_eq : RelDec (@eq func) :=
+  { rel_dec := fun a b =>
+       match a , b with
+         | fOpaque a , fOpaque b => a ?[ eq ] b
+         | fLoc sa la , fLoc sb lb => false (** TODO **)
+         | fArray sa la aa , fArray sb lb ab => false (** TODO **)
+         | fBVop sa ba , fBVop sb bb =>
+           if sa ?[ eq ] sb then ba ?[ eq ] bb else false
+         | fIf t , fIf u => t ?[ eq ] u
+         | fCast z a b , fCast w c d =>
+           if z ?[ eq ] w then
+             if a ?[ eq ] c then b ?[ eq ] d else false
+           else false
+         | fConst sa ia , fConst sb ib => false
+         | _ , _ => false
+       end }.
 
   Definition sprop := expr typ func.
   Definition sexpr := expr typ func.
 
-  Axiom int_typ : nat -> typ.
-  Axiom array_typ : nat -> nat -> typ.
-
   Definition injLoc {sz : _} (l : location sz) : func :=
-    inl (@existT _ _ sz l).
+    fLoc l.
 
-  Definition injArray (s l : _) (a : array s l) : func.
-  Admitted.
+  Definition mkCast (signed : bool) (from to : nat) : sexpr :=
+    Inj (fCast signed from to).
+  Definition mkConst (sz : _) (val : int sz) : sexpr :=
+    Inj (fConst val).
 
-  Definition bv_op (sz : nat) (o : bit_vector_op) : sexpr.
-  Admitted.
+  Definition lfalse : sprop := Inj fFalse.
+  Definition ltrue : sprop := Inj fTrue.
+  Definition is_readable (a b : sexpr) : sprop :=
+    Inj fReadable @ a @ b.
+  Definition is_writeable (a b : sexpr) : sprop :=
+    Inj fWriteable @ a @ b.
+  Definition land (a b : sprop) : sprop :=
+    Inj fAnd @ a @ b.
+  Definition lor (a b : sprop) : sprop :=
+    Inj fOr @ a @ b.
+  Definition is_true (a : sexpr) : sprop :=
+    Inj (fIf (tyInt 1)) @ a @ ltrue @ lfalse.
+  Definition lif (t : typ) (a : sexpr) (b c : sprop) : sprop :=
+    Inj (fIf t) @ a @ b @ c.
+  Definition lex (t : typ) (a : sprop) : sprop :=
+    Inj (fEx t) @ a.
+  Definition leq (t : typ) (a b : sexpr) : sprop :=
+    Inj (fEq t) @ a @ b.
 
-  Definition test_op (sz : nat) (o : test_op) : sexpr.
-  Admitted.
+  Definition array_upd (s l : nat) : sexpr := Inj (fArray_upd s l).
 
-  Definition mkIf (a b c : sexpr) : sexpr.
-  Admitted.
+  Definition memory_upd : sexpr := Inj fMem_upd.
+  Definition memory_read : sexpr := Inj fMem_read.
 
-  Axiom mkCast : bool -> nat -> nat -> sexpr.
-  Axiom mkConst : forall sz, int sz -> sexpr.
-
-  Axiom lfalse : sprop.
-  Axiom is_readable : sexpr -> sexpr -> sprop.
-  Axiom is_writeable : sexpr -> sexpr -> sprop.
-  Axiom land : sprop -> sprop -> sprop.
-  Axiom lor : sprop -> sprop -> sprop.
-  Axiom is_true : sexpr -> sprop.
-  Axiom lneg : sprop -> sprop.
-  Axiom lex : typ -> sprop -> sprop.
-  Axiom leq : typ -> sexpr -> sexpr -> sprop.
-  Axiom array_upd : nat -> nat -> sexpr.
-  Axiom the_memory : func.
-  Axiom memory_upd : sexpr.
-  Axiom memory_read : sexpr.
 
   Fixpoint rtl_exp_to_expr {sz : nat} (re : rtl_exp sz)
   : option sexpr.
@@ -66,27 +131,27 @@ Section wp.
         | arith_rtl_exp sz o l r =>
           l <- rtl_exp_to_expr _ l ;;
           r <- rtl_exp_to_expr _ r ;;
-          ret (App (App (bv_op sz o) l) r)
+          ret (Inj (fBVop sz o) @ l @ r)
         | test_rtl_exp sz t l r =>
           l <- rtl_exp_to_expr _ l ;;
           r <- rtl_exp_to_expr _ r ;;
-          ret (App (App (test_op sz t) l) r)
+          ret (Inj (fTestop sz t) @ l @ r)
         | if_rtl_exp sz test t f =>
           test <- rtl_exp_to_expr _ test ;;
           t <- rtl_exp_to_expr _ t ;;
           f <- rtl_exp_to_expr _ f ;;
-          ret (mkIf test t f)
+          ret (Inj (fIf (tyInt sz)) @ test @ t @ f)
         | cast_s_rtl_exp s1 s2 e =>
           e <- rtl_exp_to_expr _ e ;;
-          ret (App (mkCast true s1 s2) e)
+          ret (mkCast true s1 s2 @ e)
         | cast_u_rtl_exp s1 s2 e =>
           e <- rtl_exp_to_expr _ e ;;
-          ret (App (mkCast false s1 s2) e)
+          ret (mkCast false s1 s2 @ e)
         | imm_rtl_exp _ val =>
           ret (mkConst val)
         | get_byte_rtl_exp a =>
           a <- rtl_exp_to_expr _ a ;;
-          ret (memory_read @ Inj the_memory @ a)
+          ret (memory_read @ Inj fMemory @ a)
         | _ => None
       end%monad.
   Defined.
@@ -128,8 +193,6 @@ Section wp.
       end.
   End replace_array.
 
-
-  (** TODO(gmalecha): array and memory operations **)
   Fixpoint wp_rtl_instr (s : rtl_instr) (p : sprop)
   : option sprop.
   refine
@@ -140,31 +203,32 @@ Section wp.
       | set_array_rtl l s a x v =>
         x_e <- rtl_exp_to_expr x ;;
         v_e <- rtl_exp_to_expr v ;;
+        None  (*
         ret (replace_inj_sexpr
                (fun under i =>
-                  if i ?[ eq ] (injArray a) then
+                  if i ?[ eq ] (fArray a) then
                     App (App (App (array_upd l s) (Inj i))
                              (lift 0 under x_e))
                         (lift 0 under v_e)
                   else
                     Inj i)
-               0 p)
+               0 p) *)
       | set_byte_rtl e a =>
         a_e <- rtl_exp_to_expr a ;;
         e_e <- rtl_exp_to_expr e ;;
+        None (*
         ret (land (is_writeable (Inj the_memory) a_e)
                   (replace_inj_sexpr
                      (fun under i =>
                         if i ?[ eq ] the_memory then
                           memory_upd @ Inj the_memory @ lift 0 under a_e @ lift 0 under e_e
                         else Inj i)
-                     0 p))
+                     0 p)) *)
       | advance_oracle_rtl => ret p
       | if_rtl t e =>
         test_e <- rtl_exp_to_expr t ;;
         tr_e <- wp_rtl_instr e p ;;
-        ret (lor (land (is_true test_e) tr_e)
-                 (land (lneg (is_true test_e)) p))
+        ret (lif tyProp test_e tr_e p)
       | error_rtl => ret lfalse
       | trap_rtl => ret lfalse
     end%monad.
@@ -194,9 +258,10 @@ Section wp.
 
   Eval compute in
       fun (P : int size_addr) (X : int 8) (L : location 8) =>
-        wp_rtl_instrs (cons (set_loc_rtl _ (get_byte_rtl_exp (imm_rtl_exp _ P)) L) nil)
-                      (leq (int_typ 8) (mkConst X)
-                           (memory_read @ Inj the_memory @ mkConst P)).
+        wp_rtl_instrs (set_loc_rtl _ (get_byte_rtl_exp (imm_rtl_exp _ P)) L
+                       :: nil)
+                      (leq (tyInt 8) (mkConst X)
+                           (memory_read @ Inj fMemory @ mkConst P)).
 End wp.
 
 (** NOTES:
